2ba1d6400a79c33cf045444848422404ffa5da4c
damianwajser <wajser@gmail,com>
2017-11-15 10:58:26 -0300

    add log

--- src/main/java/com/github/damianwajser/model/PathVariable.java
+++ src/main/java/com/github/damianwajser/model/PathVariable.java
@@ -7,9 +7,13 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.github.damianwajser.utils.ReflectionUtils;
 
 public class PathVariable {
+	private static final Logger LOGGER = LoggerFactory.getLogger(PathVariable.class);
 	private List<Parameters> params = new ArrayList<>();
 	private static Pattern pattern = Pattern.compile("\\{[a-zA-Z0-9]*\\}");
 
@@ -17,10 +21,12 @@ public class PathVariable {
 		params = ReflectionUtils.getPathVariable(m);
 		if (!params.isEmpty()) {
 			Matcher matcher = pattern.matcher(relativePath);
+			LOGGER.info("fix path variable: " + relativePath);
 			if (matcher.find()) {
 				for (int i = 0; i < params.size(); i++) {
 					String param = matcher.group(i);
-					params.get(i).setName(param.replaceAll("\\{", "").replaceAll("\\}",""));
+					LOGGER.info("param: "+param);
+					params.get(i).setName(param.replaceAll("\\{", "").replaceAll("\\}", ""));
 				}
 			}
 		}
