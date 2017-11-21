4f588996d77801b1ae4ba0d8a5f804dcc4d544f4
damianwajser <wajser@gmail,com>
2017-11-21 13:27:31 -0300

    add tests

--- src/test/java/com/github/damianwajser/ValidatorTest.java
+++ src/test/java/com/github/damianwajser/ValidatorTest.java
@@ -1,25 +1,29 @@
 package com.github.damianwajser;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.assertNotNull;
 
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Optional;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.github.damianwajser.model.Pojo;
 import com.github.damianwajser.model.validators.Validator;
 import com.github.damianwajser.model.validators.ValidatorFactory;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 public class ValidatorTest {
 
 	@Test
 	public void contextLoads() throws Exception {
 		for (Field f:Pojo.class.getDeclaredFields()) {
 			Optional<List<Validator>> optValidators = ValidatorFactory.getValidations(f);
-//			List<Validator> validators = optValidators.get();
-//			assertNotNull(validators, "no se encontraron validators");
+			System.out.println(optValidators);
+			List<Validator> validators = optValidators.get();
+			assertNotNull("no se encontraron validators", validators);
 			
 		}
 
