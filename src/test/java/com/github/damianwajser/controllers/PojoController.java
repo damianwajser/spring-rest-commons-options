fa6c1a27dc12d33a7cc0ec21885c0959d29002f9
damianwajser <wajser@gmail,com>
2017-11-22 14:46:55 -0300

    add tests

--- src/test/java/com/github/damianwajser/controllers/PojoController.java
+++ src/test/java/com/github/damianwajser/controllers/PojoController.java
@@ -3,6 +3,7 @@ package com.github.damianwajser.controllers;
 import java.util.Collection;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.github.damianwajser.model.Pojo;
@@ -11,6 +12,11 @@ import com.github.damianwajser.model.Pojo;
 public class PojoController {
 	@GetMapping("/")
 	public Collection<Pojo> getAll() {
+		return null;
+	}
+
+	@GetMapping("/{id}")
+	public Pojo getById(@PathVariable Integer id) {
 		return null;
 	}
 }
--- src/test/java/com/github/damianwajser/test/PojoControllerTest.java
+++ src/test/java/com/github/damianwajser/test/PojoControllerTest.java
@@ -1,9 +1,9 @@
 package com.github.damianwajser.test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 import java.util.Collections;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,27 +13,62 @@ import com.github.damianwajser.builders.json.JsonBuilder;
 import com.github.damianwajser.controllers.PojoController;
 import com.github.damianwajser.model.Endpoint;
 import com.github.damianwajser.model.OptionsResult;
+import com.github.damianwajser.model.Parameters;
+import com.github.damianwajser.model.PathVariable;
 import com.github.damianwajser.model.details.DetailField;
 import com.github.damianwajser.model.details.DetailFieldCollection;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class PojoControllerTest {
-	
+
 	@Test
 	@org.junit.jupiter.api.Test
 	public void testGetAll() throws Exception {
 		JsonBuilder builder = new JsonBuilder(new PojoController());
 		OptionsResult result = builder.build().get();
 		assertEquals("/test123", result.getBaseUrl());
-		Endpoint endpoint = result.getEnpoints().get(0);
-		assertEquals("/test123", endpoint.getBaseUrl());
-		assertEquals("GET", endpoint.getHttpMethod());
-		assertEquals("/", endpoint.getRelativeUrl());
+		List<Endpoint> endpoints = result.getEnpoints();
+		for (int i = 0; i < endpoints.size(); i++) {
+			Endpoint endpoint = endpoints.get(i);
+			assertEquals("/test123", endpoint.getBaseUrl());
+			DetailField realField = null;
+			switch (endpoint.getEndpoint()) {
+			case "GET - /test123/":
+				assertEquals("/", endpoint.getRelativeUrl());
+				assertEquals(endpoint.getPathVariable().getParams(), Collections.EMPTY_LIST);
+				DetailFieldCollection field = (DetailFieldCollection) endpoint.getBodyResponse().getFields().iterator()
+						.next();
+				assertEquals("collection", field.getType());
+				realField = field.getCollection().iterator().next();
+				break;
+			case "GET - /test123/{id}":
+				assertEquals("/{id}", endpoint.getRelativeUrl());
+				assertEquals(1, endpoint.getPathVariable().getParams().size());
+				Parameters path = endpoint.getPathVariable().getParams().iterator().next();
+				assertEquals(path.getName(), "id");
+				assertEquals(path.getType(), "Integer");
+				assertEquals(1, endpoint.getBodyResponse().getFields().size());
+				realField = endpoint.getBodyResponse().getFields().iterator().next();
+				break;
+			}
+			checkRequestBodyEmpty(endpoint);
+			checkGetAllEnpoint(endpoint);
+			checkPojoField(realField);
+			assertEquals(endpoint.getQueryString().getParams(), Collections.EMPTY_LIST);
+
+		}
+	}
+
+	private void checkPojoField(DetailField realField) {
+		assertEquals("notBlank", realField.getName());
+		assertEquals("String", realField.getType());
+	}
+
+	private void checkRequestBodyEmpty(Endpoint endpoint) {
 		assertEquals(endpoint.getBodyRequest().getFields(), Collections.emptyList());
-		DetailFieldCollection field = (DetailFieldCollection)endpoint.getBodyResponse().getFields().iterator().next();
-		assertEquals("collection", field.getType());
-		assertEquals("notBlank", field.getCollection().iterator().next().getName());
-		assertEquals("String", field.getCollection().iterator().next().getType());
-	
+	}
+
+	private void checkGetAllEnpoint(Endpoint endpoint) {
+		assertEquals("GET", endpoint.getHttpMethod());
 	}
 }
